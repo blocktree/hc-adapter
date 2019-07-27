4ab00a0236be2e09f23e09e3cd32b1a76193fdc0
chance <zhiquan911@qq.com>
2019-07-27 15:01:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/blocktree/hc-adapter
+
+go 1.12
+
+require (
+	github.com/HcashOrg/bliss v0.0.0-20180719035130-f5d53c2a9b7d // indirect
+	github.com/HcashOrg/hcd v0.0.0-20190611115530-593f7eea8b25
+	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
+	github.com/asdine/storm v2.1.2+incompatible
+	github.com/astaxie/beego v1.11.1
+	github.com/blocktree/go-owcdrivers v1.0.37
+	github.com/blocktree/go-owcrypt v1.0.2
+	github.com/blocktree/openwallet v1.4.6
+	github.com/bndr/gotabulate v1.1.2
+	github.com/codeskyblue/go-sh v0.0.0-20190328095946-f4ce45e7999e
+	github.com/dchest/blake256 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.8.25
+	github.com/graarh/golang-socketio v0.0.0-20170510162725-2c44953b9b5f
+	github.com/imroc/req v0.2.3
+	github.com/pborman/uuid v1.2.0
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
+	github.com/tidwall/gjson v1.2.1
+	go.etcd.io/bbolt v1.3.3 // indirect
+)
+
+replace github.com/blocktree/go-owcdrivers => ../../go-owcdrivers
---
+++ hc/omni_contract_test.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 The openwallet Authors
+ * This file is part of the openwallet library.
+ *
+ * The openwallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package hc
+
+import (
+	"testing"
+)
+
+func TestWalletManager_GetOmniBalance(t *testing.T) {
+	propertyID := uint64(2)
+	address := "Tsa6c6ivPG3fjRnJwKjGPUyMauPYDvK6Ckk"
+	balance, err := tw.GetOmniBalance(propertyID, address)
+	if err != nil {
+		t.Errorf("GetOmniBalance failed unexpected error: %v\n", err)
+		return
+	}
+	t.Logf("balance: %v\n", balance)
+}
+
+func TestWalletManager_IsHaveOmniAssets(t *testing.T) {
+	address := "Tsa6c6ivPG3fjRnJwKjGPUyMauPYDvK6Ckk"
+	//address := "mi9qsHKMqtrgnbxg7ifdPMk1LsFmen4xNn"
+	bool := tw.IsHaveOmniAssets(address)
+	t.Logf("IsHaveOmniAssets: %v\n", bool)
+}
+
+func TestWalletManager_GetOmniTransaction(t *testing.T) {
+	txid := "300142e7f87bf669b09079cf25985f773ab4065498b15e51498533b949e054a0"
+	transaction, err := tw.GetOmniTransaction(txid)
+	if err != nil {
+		t.Errorf("GetOmniBalance failed unexpected error: %v\n", err)
+		return
+	}
+	t.Logf("transaction: %+v", transaction)
+}
+
+func TestWalletManager_GetOmniInfo(t *testing.T) {
+	result, err := tw.GetOmniInfo()
+	if err != nil {
+		t.Errorf("TestWalletManager_GetOmniInfo failed unexpected error: %v\n", err)
+		return
+	}
+	t.Logf("OmniInfo: %+v", result)
+}
+
+func TestWalletManager_GetOmniProperty(t *testing.T) {
+	propertyID := uint64(2)
+	result, err := tw.GetOmniProperty(propertyID)
+	if err != nil {
+		t.Errorf("GetOmniProperty failed unexpected error: %v\n", err)
+		return
+	}
+	t.Logf("GetOmniProperty: %+v", result)
+}
